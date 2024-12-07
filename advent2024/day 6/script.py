7ebdfa01df490bd2e9052896e786106358c39e1b
ZISHAN ANSARI <zishanza436@gmail.com>
2024-12-07 19:09:01 +0530

    finally part I solution is done

--- advent2024/day 6/script.py
+++ advent2024/day 6/script.py
@@ -1,6 +1,6 @@
 def grid(file):
     # up down right left
-    dir = ["^","V", ">", "<"] 
+    dir = ["^","V", ">", "<"] # show case the turtle
     grid = []
     with open(file) as f:
         lst = (f.readlines())      
@@ -17,6 +17,7 @@ def initial_positon(grid):
                 continue
     return[-1,-1]
 def traversel(grid,start):
+    traveled_pos = []
     jump = 1
     x , y = start
     x_inc = y_inc = 0
@@ -35,9 +36,10 @@ def traversel(grid,start):
         print("[*] No starting Position Found exiting!")
         return 
     
-    while((x <= row-1) and (y<=col-1) and (x>= 0) and (y>= 0)):#this will make sure our turtle is inside the grid
+    while((x < row-1) and (y<col-1) and (x>0) and (y>0)):#this will make sure our turtle is inside the grid        
         x_update = x  + x_inc
         y_update = y  + y_inc
+        #print(x,y,"   ",grid[x_update][y_update],turtle)
         if(grid[x_update][y_update] == "#"):
                 if(turtle == "^"):
                     x_inc, y_inc = 0,1
@@ -55,10 +57,12 @@ def traversel(grid,start):
                     x_inc, y_inc = -1,0
                     turtle = "^"
                     continue
-        jump+=1
+        elif(grid[x_update][y_update]=="." and ((x_update,y_update) not in traveled_pos)):
+            jump+=1
+            traveled_pos.append((x_update,y_update))
         x += x_inc
         y += y_inc
-    return jump+1
+    return jump
 grid_, start = grid("text.txt")
 
 print(traversel(grid_,start))
